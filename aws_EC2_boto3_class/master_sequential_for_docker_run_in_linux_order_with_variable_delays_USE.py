e2ea581b2c5a068981933162aee1da79ef34ff4f
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2025-03-22 02:02:32 +0000

    the new code to wait for running state prior to SSH is committed. Reduce teh wait state between the 5_ and 6_ script to 5 seconds so that the new code gets tested

--- aws_EC2_boto3_class/master_sequential_for_docker_run_in_linux_order_with_variable_delays_USE.py
+++ aws_EC2_boto3_class/master_sequential_for_docker_run_in_linux_order_with_variable_delays_USE.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     directory = '/aws_EC2/sequential_master' # Replace with actual directory path
     
     # Specify the delays between running each script
-    delays = [20, 360, 90, 360, 90]  # Replace with actual delay values
+    delays = [20, 5, 90, 360, 90]  # Replace with actual delay values
     # the first number in the array is delay before file 2, is the actual delay before file 3 so this is the wait time for file 2, so use 20, 360, 90 for the delay for first, second, and third files.
 # add 360,90 delays for scripts 5 and 6 for the ssh and tomcat installation
     run_python_scripts_sequentially(directory, delays)
