c9b692a6b839c49815878a2b834e773e184882e6
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2025-03-31 23:03:50 +0000

    forgot to add tomcat_target_group_arn to teh 9_ script for current boto3 session state

--- aws_EC2_boto3_class/sequential_master/9_autoscaling_group.py
+++ aws_EC2_boto3_class/sequential_master/9_autoscaling_group.py
@@ -36,6 +36,18 @@ route53_client = session.client('route53')
 autoscaling_client = session.client('autoscaling')
 
 
+# Retrieve the target group ARN for Tomcat instances
+target_groups = elb_client.describe_target_groups(LoadBalancerArn=load_balancer_arn)
+tomcat_target_group_arn = None
+for tg in target_groups['TargetGroups']:
+    if 'tomcat' in tg['TargetGroupName'].lower():
+        tomcat_target_group_arn = tg['TargetGroupArn']
+        break
+
+if not tomcat_target_group_arn:
+    logger.error("Tomcat target group ARN not found.")
+    raise ValueError("Tomcat target group ARN not found.")
+
 
 # Load instance and security group IDs from JSON file
 with open('instance_ids.json', 'r') as f:
