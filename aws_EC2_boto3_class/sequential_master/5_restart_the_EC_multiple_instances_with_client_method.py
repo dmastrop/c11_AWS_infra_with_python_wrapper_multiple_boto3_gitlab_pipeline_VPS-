b1298c0229677cb47a6c1e7a6439fa1923e00dfe
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2025-04-14 02:35:11 +0000

    add the EC2 instance in 5_ code with error handling and better printing

--- aws_EC2_boto3_class/sequential_master/5_restart_the_EC_multiple_instances_with_client_method.py
+++ aws_EC2_boto3_class/sequential_master/5_restart_the_EC_multiple_instances_with_client_method.py
@@ -20,29 +20,94 @@ min_count = f'{os.getenv("min_count")}'
 max_count = f'{os.getenv("max_count")}'
 
 
+#def start_ec2_instances(aws_access_key, aws_secret_key, region_name, image_id, instance_type, key_name, min_count, max_count):
+#    # Establish a session with AWS
+#    session = boto3.Session(
+#        aws_access_key_id=aws_access_key,
+#        aws_secret_access_key=aws_secret_key,
+#        region_name=region_name
+#    )
+#    
+#    # Create an EC2 client
+#    my_ec2 = session.client('ec2')
+#    
+#    # Start EC2 instances
+#    response = my_ec2.run_instances(
+#        ImageId=image_id,
+#        InstanceType=instance_type,
+#        KeyName=key_name,
+#        MinCount=int(min_count),
+#        MaxCount=int(max_count)
+#    )
+#    
+#    return response
+#
+#
+#response = start_ec2_instances(aws_access_key, aws_secret_key, region_name, image_id, instance_type, key_name, min_count, max_count)
+#print(response)
+
+
+
+
+
+
+
+## Put the function in with the error handling and easy to read print outs:
 def start_ec2_instances(aws_access_key, aws_secret_key, region_name, image_id, instance_type, key_name, min_count, max_count):
     # Establish a session with AWS
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key,
-        aws_secret_access_key=aws_secret_key,
-        region_name=region_name
-    )
-    
+    try:
+        session = boto3.Session(
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key,
+            region_name=region_name
+        )
+        print("AWS session established.")
+    except Exception as e:
+        print("Error establishing AWS session:", e)
+        sys.exit(1)
+
     # Create an EC2 client
-    my_ec2 = session.client('ec2')
-    
+    try:
+        my_ec2 = session.client('ec2')
+        print("EC2 client created.")
+    except Exception as e:
+        print("Error creating EC2 client:", e)
+        sys.exit(1)
+
     # Start EC2 instances
-    response = my_ec2.run_instances(
-        ImageId=image_id,
-        InstanceType=instance_type,
-        KeyName=key_name,
-        MinCount=int(min_count),
-        MaxCount=int(max_count)
-    )
-    
+    try:
+        response = my_ec2.run_instances(
+            ImageId=image_id,
+            InstanceType=instance_type,
+            KeyName=key_name,
+            MinCount=int(min_count),
+            MaxCount=int(max_count)
+        )
+        print("EC2 instances started:", response)
+    except Exception as e:
+        print("Error starting EC2 instances:", e)
+        sys.exit(1)
+
     return response
 
-
 response = start_ec2_instances(aws_access_key, aws_secret_key, region_name, image_id, instance_type, key_name, min_count, max_count)
-print(response)
+#print(response)
 
+
+# Print the response in a more readable format using json.dumps for pretty printing
+#print(json.dumps(response, indent=4))
+
+
+
+# Print the response in a more readable format
+if 'Instances' in response:
+    for i, instance in enumerate(response['Instances']):
+        print(f"Instance {i+1}:")
+        print(f"  Instance ID: {instance['InstanceId']}")
+        print(f"  Instance Type: {instance['InstanceType']}")
+        print(f"  Image ID: {instance['ImageId']}")
+        print(f"  State: {instance['State']['Name']}")
+        print(f"  Private IP Address: {instance['PrivateIpAddress']}")
+        print(f"  Subnet ID: {instance['SubnetId']}")
+else:
+    print("No instances found in the response.")
